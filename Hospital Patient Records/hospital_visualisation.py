cd24ef0dc3dede977e3a0fb66e143ca73b798dda
Sfundo <sfundon97@gmail.com>
2024-07-22 13:46:53 +0200

    Final Commit

--- Hospital Patient Records/hospital_visualisation.py
+++ Hospital Patient Records/hospital_visualisation.py
@@ -5,9 +5,27 @@ import seaborn as sns
 def main():
     # Read the CSV file
     df = pd.read_csv("Hospital Patient Records/hospital.csv")
+    print('Dataframe Contents')
+    print('==================================================================')
     print(df)
-   
-    
+    print('------------------------------------------------------------------')
+    print()
+    print('Summary of the DataFrame, including the number of non-null records and data types')
+    print('==================================================================')
+    print(df.info())
+    print('------------------------------------------------------------------')
+    print()
+    print('Summary statistics (like count, mean, min, max, etc.)')
+    print('==================================================================')
+    print(df.describe())
+    print('------------------------------------------------------------------')
+    print()
+    print('The Number of Missing Data represented by 1 = True and 0 = False:')
+    print('=================================================================')
+    print(df.isnull().sum())
+    print('------------------------------------------------------------------')
+
+  
     # Bar chart of counts by gender
     plt.figure(figsize=(8, 6))
     sns.countplot(x='Gender', data=df)
@@ -42,6 +60,21 @@ def main():
     plt.ylabel('Medical Condition')
     plt.legend(title='Gender')
     plt.show()
+
+    # Histogram of bill amounts
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df['Bill Amount'], kde=True)
+    plt.title('Distribution of Bill Amounts')
+    plt.xlabel('Bill Amount')
+    plt.ylabel('Frequency')
+    plt.show()
+
+    median_amount = df['Bill Amount'].median(axis=0)
+    mode_gender = df['Gender'].mode()
+    print('Median Bill: ', median_amount)
+    print('Gender that falls sick the most:', mode_gender)
+    print('------------------------------------------------------------------')
+    print()
 
 
 main()
